af4ba1160e10ae67159c3c2347e79d58dbfc0178
smondi <siva.mondi@iexsoft.com>
2014-06-01 23:33:02 -0400

    updated

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,35 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import models.Group;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Application extends Controller {
+	static Form<Group> groupForm = Form(Group.class);
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() throws Exception {
+		// redirect to the "group Result
+		return redirect(routes.Application.group());
+	}
+
+	public static Result group() {
+		return ok(views.html.index.render(Group.all(), groupForm));
+	}
+
+	public static Result newGroup() {
+		Form<Group> filledForm = groupForm.bindFromRequest();
+		if(filledForm.hasErrors()) {
+			return badRequest(views.html.index.render(Group.all(), filledForm));
+		} else {
+			Group.create(filledForm.get());
+			return redirect(routes.Application.group());  
+		}
+	}
+	
+	public static Result deleteGroup(String id) {
+		Group.delete(id);
+		return redirect(routes.Application.group());
+	}
 
 }
